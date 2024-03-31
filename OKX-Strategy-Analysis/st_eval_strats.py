6dd8d49cd9f66b1fb169c1e4f368cb39ed0b7030
Eric Ervin <eervin@blockforcecapital.com>
2024-03-31 07:32:38 -0700

    improved streamlit app now showing much more

--- OKX-Strategy-Analysis/st_eval_strats.py
+++ OKX-Strategy-Analysis/st_eval_strats.py
@@ -31,3 +31,28 @@ with col1:
 # Display the plot in the second column
 with col2:
     st.plotly_chart(pf.plot(width=600, height=900))
+
+
+# Trade Analysis
+st.subheader("Trade Analysis")
+st.plotly_chart(pf.plot_trade_signals())
+
+# Create two columns for stats and plot
+col3, col4 = st.columns([3,4])
+width = 500
+with col3:
+    st.plotly_chart(pf.trades.plot_expanding_mae_returns(title='Worst Trades', width=width)) # This is the maximum adverse excursion (MAE)
+    st.plotly_chart(pf.trades.plot_mae_returns(title='Worst Trades', width=width))
+with col4:
+    st.plotly_chart(pf.trades.plot_expanding_mfe_returns(title='Best Trades', width=width)) # This is the maximum favorable excursion (MFE)
+    st.plotly_chart(pf.trades.plot_mfe_returns(title='Best Trades', width=width))
+    
+# Give the user the option to see all the trades
+if st.checkbox('Show all trades'):
+    st.dataframe(pf.trade_history)
+
+if st.checkbox('Show detailed trade records'):
+    st.dataframe(pf.trades.records_readable)
+    
+if st.checkbox('Show all orders'):
+    st.dataframe(pf.orders.records_readable)
